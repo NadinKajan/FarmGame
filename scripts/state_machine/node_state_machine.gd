ad0fbe7ec122fe2b03e3c0a3d82954e77a71da1c
NadinKajan1 <nadinnnk@gmail.com>
2025-12-08 04:01:20 -0500

    Added door functionality.

--- scripts/state_machine/node_state_machine.gd
+++ scripts/state_machine/node_state_machine.gd
@@ -1,29 +1,30 @@
 class_name NodeStateMachine
 extends Node
 
-@export var initial_node_state : NodeState
+@export var initial_node_state: NodeState
 
-var node_states : Dictionary = {}
-var current_node_state : NodeState
-var current_node_state_name : String
-var parent_node_name: String
+var node_states: Dictionary[String, NodeState] = {}
+var current_node_state: NodeState
+var current_node_state_name: String = ""
+var parent_node_name: String = ""
 
 
 func _ready() -> void:
 	parent_node_name = get_parent().name
-	
+
 	for child in get_children():
 		if child is NodeState:
-			node_states[child.name.to_lower()] = child
+			var key := _get_state_key(child.name)
+			node_states[key] = child
 			child.transition.connect(transition_to)
-	
+
 	if initial_node_state:
-		initial_node_state._on_enter()
 		current_node_state = initial_node_state
-		current_node_state_name = current_node_state.name.to_lower()
+		current_node_state_name = _get_state_key(current_node_state.name)
+		current_node_state._on_enter()
 
 
-func _process(delta : float) -> void:
+func _process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_process(delta)
 
@@ -32,23 +33,27 @@ func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
 		current_node_state._on_next_transitions()
-		#print(parent_node_name, " Current State: ", current_node_state_name)
+		# print(parent_node_name, " Current State: ", current_node_state_name)
 
 
-func transition_to(node_state_name : String) -> void:
-	if node_state_name == current_node_state.name.to_lower():
+func transition_to(node_state_name: String) -> void:
+	var key := _get_state_key(node_state_name)
+
+	if current_node_state and key == current_node_state_name:
 		return
-	
-	var new_node_state = node_states.get(node_state_name.to_lower())
-	
-	if !new_node_state:
+
+	var new_node_state: NodeState = node_states.get(key)
+	if new_node_state == null:
 		return
-	
+
 	if current_node_state:
 		current_node_state._on_exit()
-	
-	new_node_state._on_enter()
-	
+
 	current_node_state = new_node_state
-	current_node_state_name = current_node_state.name.to_lower()
-	#print("Current State: ", current_node_state_name)
+	current_node_state_name = key
+	current_node_state._on_enter()
+	# print("Current State: ", current_node_state_name)
+
+
+func _get_state_key(name: String) -> String:
+	return name.to_lower()
